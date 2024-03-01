21fd8612741010f16a55751f1c678c430526fdbe
deltaonlyone <deltaonlyone@gmail.com>
2024-03-02 01:52:47 +0200

    created DAO User

---
+++ src/main/java/com/twovtwok/backend/dao/User.java
@@ -0,0 +1,27 @@
+package com.twovtwok.backend.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.NonNull;
+
+@Entity(name="users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    private String username;
+    @NonNull
+    private String password;
+    @NonNull
+    private String permissions;
+}
